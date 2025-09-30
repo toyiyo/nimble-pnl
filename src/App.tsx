1a4385daf0dc1afda74458750c8bd45d5c7d9127
Jose M Delgado <jdelgado2002@gmail.com>
2025-09-30 21:58:50 +0000

    Add SpeedInsights component to App for enhanced performance tracking

--- src/App.tsx
+++ src/App.tsx
@@ -61,6 +61,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <Analytics />
+        <SpeedInsights />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<ProtectedRoute><Index /></ProtectedRoute>} />

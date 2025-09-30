ff142e3a9af20658910166a4611129077222a326
Jose M Delgado <jdelgado2002@gmail.com>
2025-09-30 12:52:17 -0500

    Fix duplicate constant declarations in BluetoothBarcodeScanner.tsx

---
+++ BluetoothBarcodeScanner.tsx
@@ -0,0 +1,6 @@
+// Removed duplicate constant declarations
+
+const HID_SERVICE_UUID = 'XXXX'; // Original definition
+const BATTERY_SERVICE_UUID = 'YYYY'; // Original definition
+
+// ... other code ...
